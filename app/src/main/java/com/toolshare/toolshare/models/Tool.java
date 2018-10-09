ef0e91b95f6735405d2baec9281ceddffdaae485
Angela <angelaranola30@gmail.com>
2018-10-09 14:38:30 -0600

    Added Tool model

---
+++ app/src/main/java/com/toolshare/toolshare/models/Tool.java
@@ -0,0 +1,76 @@
+package com.toolshare.toolshare.models;
+
+public class Tool {
+    private int Id;
+    private String Owner;
+    private int TypeId;
+    private String Name;
+    private int Year;
+    private String Model;
+
+    public Tool() {
+        this.Id = -1;
+        this.Owner = null;
+        this.TypeId = -1;
+        this.Name = null;
+        this.Year = -1;
+        this.Model = null;
+    }
+    
+    public Tool(int id, String owner, int typeId, String name, int year, String model) {
+        this.Id = id;
+        this.Owner = owner;
+        this.TypeId = typeId;
+        this.Name = name;
+        this.Year = year;
+        this.Model = model;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        this.Id = id;
+    }
+
+    public String getOwner() {
+        return Owner;
+    }
+
+    public void setOwner(String owner) {
+        this.Owner = owner;
+    }
+
+    public int getTypeId() {
+        return TypeId;
+    }
+
+    public void setTypeId(int typeId) {
+        this.TypeId = typeId;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        this.Name = name;
+    }
+
+    public int getYear() {
+        return Year;
+    }
+
+    public void setYear(int year) {
+        this.Year = year;
+    }
+
+    public String getModel() {
+        return Model;
+    }
+
+    public void setModel(String model) {
+        this.Model = model;
+    }
+}
