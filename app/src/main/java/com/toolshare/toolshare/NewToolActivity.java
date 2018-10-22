33586b9d9f2d6a2df3edcf0cbf9b2e3c472dc21d
Angela <angelaranola30@gmail.com>
2018-10-21 19:08:14 -0600

    Added New Tool page

--- app/src/main/java/com/toolshare/toolshare/NewToolActivity.java
+++ app/src/main/java/com/toolshare/toolshare/NewToolActivity.java
@@ -1,16 +1,15 @@
 package com.toolshare.toolshare;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.PopupMenu;
 import android.widget.TextView;
 
-public class ProfileActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
+public class NewToolActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
 
@@ -25,7 +24,7 @@ public class ProfileActivity extends AppCompatActivity implements PopupMenu.OnMe
                     return true;
                 case R.id.navigation_add:
                     mTextMessage.setText("Add");
-                    PopupMenu popup = new PopupMenu(ProfileActivity.this, findViewById(R.id.navigation_add));
+                    PopupMenu popup = new PopupMenu(NewToolActivity.this, findViewById(R.id.navigation_add));
                     MenuInflater inflater = popup.getMenuInflater();
                     inflater.inflate(R.menu.add_menu, popup.getMenu());
                     popup.show();
@@ -44,24 +43,6 @@ public class ProfileActivity extends AppCompatActivity implements PopupMenu.OnMe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
-
-    @Override
-    public boolean onMenuItemClick(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.new_tool_option:
-                Intent i = new Intent(ProfileActivity.this, NewToolActivity.class);
-                startActivity(i);
-                return true;
-            case R.id.new_ad_option:
-                return true;
-            default:
-                return false;
-        }
+        setContentView(R.layout.activity_new_tool);
     }
 }
