a1185fa4bb18f460e52bce6b73a74a908fbe2977
Angela <angelaranola30@gmail.com>
2018-10-21 19:00:54 -0600

    Added popup menu for adding tools and ads

--- app/src/main/java/com/toolshare/toolshare/ProfileActivity.java
+++ app/src/main/java/com/toolshare/toolshare/ProfileActivity.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
@@ -20,8 +22,15 @@ public class ProfileActivity extends AppCompatActivity {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
                     return true;
+                case R.id.navigation_add:
+                    mTextMessage.setText("Add");
+                    PopupMenu popup = new PopupMenu(ProfileActivity.this, findViewById(R.id.navigation_add));
+                    MenuInflater inflater = popup.getMenuInflater();
+                    inflater.inflate(R.menu.add_menu, popup.getMenu());
+                    popup.show();
+                    return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    mTextMessage.setText("Browse");
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
