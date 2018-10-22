07f7c960c865ea8ad73a6293cf7cecdb632b5c02
Angela <angelaranola30@gmail.com>
2018-10-21 18:49:35 -0600

    Profile button on menu goes to Profile page

--- app/src/main/java/com/toolshare/toolshare/MainActivity.java
+++ app/src/main/java/com/toolshare/toolshare/MainActivity.java
@@ -1,42 +1,36 @@
 package com.toolshare.toolshare;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
+    private Button mProfileButton;
+    private Button mRentToolsButton;
+    private Button mRentOutToolsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        mProfileButton = findViewById(R.id.profile_button);
+        mProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(i);
+            }
+        });
+        mRentToolsButton = findViewById(R.id.rent_tools_button);
+        mProfileButton = findViewById(R.id.rent_out_tools_button);
     }
 
 }
